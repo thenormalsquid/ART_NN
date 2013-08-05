a5bfbe0159efa8207049d595c430f1c21bae823f
radicalcakes <tbhuynh10@gmail.com>
2013-08-05 15:15:29 -0400

    first commit

---
+++ code/neuralnetworks/ART.py
@@ -0,0 +1,154 @@
+import numpy
+
+#perhaps make an ART class in which all of these functions are methods
+
+class Art(object):
+    def __init__(self, oinput, weight):
+        #weight gets adjusted, maybe don't need to add weight here
+
+
+
+#may have to change to numpy array if data is large
+def category_activation(oinput, weight, bias):
+    #weight is a list of lists which converts to a matrix 
+    #oinput is a vector, which is a list
+
+    """
+    ART_Activate_Categories    Activates the categories in an ART network.
+       CATEGORYACTIVATION = ART_Activate_Categories(INPUT, WEIGHT, BIAS)
+        This function returns a vector of category activations for the given
+        input vector, weight matrix, and bias value.
+     
+        The input parameters are as follows:
+        The INPUT is a vector of size NumFeatures that contains the input
+        signal into the network. 
+        The WEIGHT is a matrix of size NumFeatures-by-NumCategories which holds the weights of the network.
+        The BIAS is the constant that is used to differentiate between very
+        similar category activation values. The length of the INPUT vector
+        must equal the number of rows in the WEIGHT matrix, and the BIAS
+        value must be within the range [0, 1] (although values very near
+        0 are best).
+
+        The return parameter is as follows:
+        The CATEGORYACTIVATION is a vector of size NumCategories that
+        holds the activation value for each category. Returns an array.
+     """
+    num_categories = len(weight[0])
+    if len(oinput) != len(weight):
+        return 'The length of the input and rows of the weights do not match.'
+    elif bias < 0 or bias > 1:
+        return "The bias must be within the range of [0,1]"
+    else:
+        """
+        Calculate the activation for each category.
+        This is done according to the following equation:
+        Activation(j) = |Input^Weight(j)| / (bias + |Weight(j)|)
+        """
+        cat = []
+        #weight should be numpy matrix already, but if not, we convert here
+        weight = numpy.matrix(weight)
+        for j in xrange(num_categories):
+            arr = weight[:,j].tolist()
+            col = column(arr,0)
+            match_vector = min(oinput, col)
+            weight_length = sum(col)
+            cat.append(sum(match_vector)/(bias + weight_length))
+    return cat
+
+
+#returns a list of matrix columns
+def column(matrix, i):
+    return [row[i] for row in matrix]
+
+
+def add_new_category(weight):
+    """
+     ART_Add_New_Category    Adds a new category to the given weight matrix.
+    RESIZEDWEIGHT = ART_Add_New_Category(WEIGHT)
+    This function returns a new weight matrix which is identical to the
+    given weight matrix except that it contains one more category which
+    is initialized to all 1's.
+ 
+    The input parameter is as follows:
+    The WEIGHT is a matrix of size NumFeatures-by-NumCategories which
+    holds the weights of the network.
+    The return parameter is as follows:
+    The RESIZEDWEIGHT is a matrix of size NumFeatures-by-NumCategories+1
+    which holds the weights of the old matrix plus a new category of all
+    values of 1.
+     """
+    resized_weight = [row.append(1) for row in weight]
+    return resized_weight
+
+
+
+
+
+def calculate_match(oinput, weight_vector):
+    """
+    ART_Calculate_Match    Calculates the match value of an input to a category.
+    MATCH = ART_Calculate_Match(INPUT, WEIGHTVECTOR)
+    This function returns a value which represents the amount of match
+    between the given input and the given category.
+ 
+    The input parameters are as follows:
+    The INPUT is a vector of size NumFeatures that contains the input
+    signal into the network. The WEIGHTVECTOR is a matrix of size 
+    NumFeatures which holds the weights of the network for a given
+    category. The length of the INPUT vector must equal the length of
+    the WEIGHTVECTOR.
+
+    The return parameter is as follows:
+    The MATCH is a measure of the degree of match between the input
+    and the current category.
+    """
+    #weight_vector is a single row matrix
+    num_features = len(oinput)
+    if num_features == len(weight_vector):
+        #Match = |Input^WeightVector| / |Input|
+        match_vector = min(oinput, weight_vector)
+        input_length = sum(oinput)
+        if input_length == 0:
+            match = 0
+        else:
+            match = sum(match_vector) / input_length
+        return match
+    else:
+        return "Input and weight_vector lengths don't match"
+
+
+def categorize(art_network, data):
+    """
+     ART_Categorize    Uses an ART network to categorize the given input data.
+    CATEGORIZATION = ART_Categorize(ART_NETWORK, DATA)
+    This function uses an ART network to categorize the given input data with 
+    the specified vigilance parameter. Each sample of the data is presented to
+    the network, which categorizes each sample. The function returns the 
+    categorization of each sample. If the categorization of the sample requires
+    that a new category be created, the category for that sample is set to -1.
+
+    The input parameters are as follows:
+    The ART_NETWORK is the trained ART network. It should be created with
+    ART_Create_Network(). -> most likely object
+
+    The DATA is the categorization data to be presented
+    to the network. It is a matrix of size NumFeatures-by-NumSamples. 
+
+    The return parameters are as follows:
+    The CATEGORIZATION is a vector of size NumSamples that holds the 
+    category in which the ART network placed each sample. -> returns list
+    """
+    #num_features will go into an art network object > may need to make the object
+    if num_features == art_network.num_features:
+        if art_network.vigilance <= 0 or art_network.vigilance > 1:
+            return "The range must be (0,1)"
+        else:
+
+
+
+#tests
+if __name__ == "__main__":
+    oin = [1,2,3,4]
+    weight = [[3,5,1,2],[4,5,2,1],[1,2,3,3],[2,3,3,2]]
+    bias = 0.1
+    print category_activation(oin, weight, bias)
